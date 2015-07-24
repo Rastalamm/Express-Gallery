0b8629e4d27dbadd5ee290c3e871ca79f0cf930b
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-23 20:31:28 -1000

    Ajax not working.. unsure of what is going on

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,24 @@
-console.log($);
+
+
+// Attach a submit handler to the form
+$( "#addPhotoForm" ).submit(function( event ) {
+
+  // Stop form from submitting normally
+  event.preventDefault();
+
+  // Get some values from elements on the page:
+  var $form = $( this ),
+    author = $form.find( "input[name='author']" ).val(),
+    link = $form.find( "input[name='link']" ).val(),
+    description = $form.find( "input[name='description']" ).val(),
+    url = $form.attr( "action" );
+
+  // Send the data using post
+  var posting = $.post( url, { author : author, link : link, description : description} );
+
+  // Put the results in a div
+  posting.done(function( data ) {
+    var content = $( data ).find( "#content" );
+    $( "#testingAjax" ).empty().append( content );
+  });
+});
